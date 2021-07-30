9cb89cf840916b7ac33bb4a8c7b476adac4ee5b1
SamuelCoolMan <88205217+SamuelCoolMan@users.noreply.github.com>
2021-07-30 10:27:33 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    scene.setBackgroundImage(assets.image`Jungle`)
+    mySprite2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+})
+let mySprite2: Sprite = null
+scene.setBackgroundImage(assets.image`Start screen`)
+let mySprite = sprites.create(assets.image`You`, SpriteKind.Player)
+controller.moveSprite(mySprite, 50, 50)
